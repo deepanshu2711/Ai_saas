bb48bc621d370b85abc53e4e02a7ab333d6fb18f
deepanshu2711 <deepanshusaini2711@gmail.com>
2024-01-13 16:15:42 +0530

    create first template and folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Dashboard() {
+  return (
+    <div className="text-3xl text-green-500">
+     <p> Dashboard (Protected) </p>
+    </div>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-green-500">
+      <p>Landing UnProtected</p>
+      
+    </div>
+  )
+}
